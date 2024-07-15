0b19f1c039a84fa6eb83f7e492a92ea00f6f3e51
PnikFlod6021 <sri062106@outlook.com>
2024-07-15 02:41:25 -0500

    changed forward function to use layers instead of neurons

--- Network.py
+++ Network.py
@@ -9,22 +9,20 @@ class Network():
     def __init__(self, sizes):
         self.sizes = sizes
         self.numLayers = len(sizes)
-        
         self.layers = [Layer(self.sizes[i]) for i in range(self.numLayers)]
     
-    # a = current activation function
-    def forward(self, a):
-        for w,b in zip(self.weights, self.biases):
-            a = sigmoid(np.dot(w,a) + b)
-        return a
-    
-    def forwardMat(self):
-        for i in range(self.numLayers-2): # for each hidden layer
-            
-            
-            # update layer
+    def forward(self, inputs):
+        for i in range(1,len(self.layers)):
+            inputs = self.layers[i].forward_prop
+        return inputs
             
 
 
 
 ## Calc output for a single nueron given weights, bias, and inputs
+
+# a = current activation function
+    # def forward(self, a):
+    #     for w,b in zip(self.weights, self.biases):
+    #         a = sigmoid(np.dot(w,a) + b)
+    #     return a
