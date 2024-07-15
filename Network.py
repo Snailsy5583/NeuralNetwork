2605e4e6c8169c4c5171131f298df24cdb2b1f29
Unpaid-Intern246 <chan.the.man1907@gmail.com>
2024-07-15 14:51:06 -0500

    Tried to simplify forward, but missing things started some wierd back prop/cost but need input data and such to be formatted so I can build off of it

--- Network.py
+++ Network.py
@@ -2,7 +2,7 @@ import random
 import numpy as np
 
 from Layer import *
-from ActivationFunc import *
+from ActivationFunc import ActivationFunction as AF   
 
 class Network():
     #sizes is list of nums for number of neurons at each layer
@@ -15,14 +15,22 @@ class Network():
         for i in range(1,len(self.layers)):
             inputs = self.layers[i].forward_prop
         return inputs
-            
+    
+
+
+    def prop(w, b, X):
+        Z = AF.sigmoid(np.dot(w.T, X) + b)
+        cost = ...
+
+        #back
 
 
 
 ## Calc output for a single nueron given weights, bias, and inputs
 
 # a = current activation function
-    # def forward(self, a):
-    #     for w,b in zip(self.weights, self.biases):
-    #         a = sigmoid(np.dot(w,a) + b)
-    #     return a
+#     def forward(self, a):
+#         for w,b in zip(self.weights, self.biases):
+#             a = sigmoid(np.dot(w,a) + b)
+#         return a
+
