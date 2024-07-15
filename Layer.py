fddf81b1a0b61deb2249c50f94c60a64d0373cf3
Snailsy <r6awesome@gmail.com>
2024-07-15 01:59:55 -0500

    moved sigmoid to neuron

--- Layer.py
+++ Layer.py
@@ -1,9 +1,6 @@
-import Neuron
+from Neuron import *
 from DataPreparation import DataPreparation
 import numpy as np
-
-def sigmoid(x):
-        return 1.0/(1.0+np.exp(-x))
 
 class Layer:
 
--- Neuron.py
+++ Neuron.py
@@ -1,5 +1,9 @@
 import numpy as np
 
+def sigmoid(x):
+        return 1.0/(1.0+np.exp(-x))
+
+
 class Neuron():
     def __init__(self):
         self.value = 0
